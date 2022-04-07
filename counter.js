1807ff010dddcff1e5fcb90df9b225eb0801018b
Bukkiee20 <adeyooyebukola22@gmail.com>
2022-04-07 15:05:24 +0100

    first commit

---
+++ counter.js
@@ -0,0 +1,33 @@
+var resultValue= document.getElementById("resultValue");
+
+let score= 0;
+function decrease(){
+    score--;
+    resultValue.innerHTML= score;
+    if(score < 1){
+        resultValue.style.color= "red";
+    }
+    else if(score==0){
+        resultValue.style.color= "black";
+    }
+}
+
+function reset(){
+   score = 0;
+   resultValue.innerHTML= score;
+   if(score == 0){
+    resultValue.style.color= "black";
+  }
+}
+
+function increase(){
+   score++;
+   resultValue.innerHTML= score;
+   if(score >=1){
+      resultValue.style.color= "green";
+    }
+   else if(score==0){
+       resultValue.style.color= "black";
+    }
+}
+
